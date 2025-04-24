979ae727062dd63104a480e3e6d65fd0b5b06486
Dimaster159 <remsma702@yandex.com>
2025-04-25 00:20:57 +0500

    1.1.4

---
+++ project1/src/js/function.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    let c = (a + b) * 5;
+    return c;
+}
+let sum1 = sum(5, 26);
+console.log(sum1);
--- project1/src/js/object.js
+++ project1/src/js/object.js
@@ -5,4 +5,41 @@ user.prof = 'frontend';
 
 delete user.prof; // Удаление объекта
 
-console.log(user)
+const city = 'myCity';
+user[city] = 'Ufa';
+user['hasCar'] = true;
+
+const house = {
+    floors: 2,
+    square: 102.5,
+}
+user['house'] = house;
+
+const car = 'Lada';
+const model = 'Vesta';
+
+const cars = {
+    car,
+    model,
+    myCar: function () {         // Функция в объекте
+        console.log(this.car);
+    },
+    myModel() {                  // Короткая запись функции
+        console.log(this.model)
+    }
+}
+
+// Копирование объекта
+
+const copyUser1 = Object.assign({}, user); // Ссылки на вложенные объекты сохраняються
+const copyUser2 = {...user}; // Ссылки на вложенные объекты сохраняються
+const copyUser3 = JSON.parse(JSON.stringify(user));
+
+// globalThis - глобальный объект
+
+console.log(copyUser1)
+console.log(user);
+console.log(user.age);
+console.log(user.house.floors);
+cars.myCar();
+cars.myModel();
